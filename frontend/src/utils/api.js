35df661a63cb4171ab463af7c30b6513d12f913e
Billu <141833342+anshuman04-oss@users.noreply.github.com>
2026-08-30 01:20:42 +0530

    Updated API base URL to include versioning

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://businessintelligenceai-uirv.onrender.com',
+  baseURL: 'https://businessintelligenceai-uirv.onrender.com/api/v1',
 });
 
 api.interceptors.request.use((config) => {

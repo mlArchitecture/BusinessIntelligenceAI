95173dae4b19cdf722ea607cfc947d7697b499b2
Billu <141833342+anshuman04-oss@users.noreply.github.com>
2026-08-30 01:35:40 +0530

    Updated CORS middleware to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,8 +24,7 @@ app = FastAPI(title="BusinessIntelligence.AI Engine")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://main.d3s8curp1ny75.amplifyapp.com",
-        "http://localhost:3000",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],

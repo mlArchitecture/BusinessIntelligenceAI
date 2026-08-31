980253e317f8a35c56010b0104bfd8f21a921fe5
Billu <141833342+anshuman04-oss@users.noreply.github.com>
2026-08-31 17:12:13 +0530

    Dummy commit to deploy frontend on Vercel

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://businessintelligenceai-uirv.onrender.com/api/v1',
 });
-
+ 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {

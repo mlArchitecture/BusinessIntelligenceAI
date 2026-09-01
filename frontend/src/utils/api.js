0cbd3e895dc8d97c432da840a7aca49266a3af59
anshuman04-oss <acharyaanshuman04@gmail.com>
2026-09-01 01:26:53 -0700

    Backend throwing error currently. To be handled.

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// baseURL: 'https://businessintelligenceai-uirv.onrender.com/api/v1'
 const api = axios.create({
-  baseURL: 'https://businessintelligenceai-uirv.onrender.com/api/v1',
+  baseURL: 'http://127.0.0.1:8000/api/v1',
 });
  
 api.interceptors.request.use((config) => {
